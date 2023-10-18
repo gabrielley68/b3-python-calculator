ccf692b8df88d8b5290e222b9f6f30de9fd38420
Ardit Mataj <ardit.mataj@my-digital-school.org>
2023-10-18 11:30:45 +0200

    code fini

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -31,8 +31,8 @@ def reste(a, b):
 # 7. Retourne le nombre sans tenir compte de son signe
 def valeur_absolue(a):
     nombre_absolu = abs(a)
+    print("Votre nombre est: ", nombre_absolu)
     return nombre_absolu
-    pass
 
 
 # 8. Retourne la valeur au carré
